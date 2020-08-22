b60720153041bc965ea0f960f6f9a93507a78b66
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-22 21:41:32 +0300

    added style.css

--- dentist/settings.py
+++ dentist/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #MEDIA_URL = '/media/'
