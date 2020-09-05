0e9c1e37eb4d01729858f95f92e307f3f770c3d0
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-05 18:26:14 +0300

    testing emails

--- dentist/settings.py
+++ dentist/settings.py
@@ -139,6 +139,13 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 '''
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cleophas.mugeni@gmail.com'
+EMAIL_HOST_PASSWORD = 'kakosh#123'
+
 
 
 django_heroku.settings(locals())
