ac6ae23ca236d775bc71c6b13d7af0c59fe41319
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-05 17:46:10 +0300

    testing emails

--- dentist/settings.py
+++ dentist/settings.py
@@ -130,13 +130,21 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
+'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+'''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cleophas.mugeni@gmail.com'
+EMAIL_HOST_PASSWORD = 'kakosh@123'
 
 
 
