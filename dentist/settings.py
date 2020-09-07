7bb9cd8fcbae50f7ded72da8df4dd9211b8e7d55
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-07 15:25:48 +0300

    testing emails

--- dentist/settings.py
+++ dentist/settings.py
@@ -130,15 +130,6 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-'''
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-'''
-
 
 
 django_heroku.settings(locals())
