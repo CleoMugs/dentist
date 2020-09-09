7050d37ed02d61168612437b04625d928365e27c
CleoMugs <cleophas.mugeni@gmail.com>
2020-09-09 16:14:26 +0300

    added postrgresql

--- dentist/settings.py
+++ dentist/settings.py
@@ -78,13 +78,28 @@ WSGI_APPLICATION = 'dentist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'menocare',
+        'USER':'cleo',
+        'PASSWORD':'kakosh123',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
